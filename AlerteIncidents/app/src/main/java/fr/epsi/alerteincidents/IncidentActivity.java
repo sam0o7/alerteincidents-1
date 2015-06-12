4fd843b18240c3cac9acffc1da3d9a2ffe27f27c
barbok211 <barbarianz@hotmail.fr>
2015-06-12 14:39:16 +0200

    Gradle
    New project with gradle

---
+++ AlerteIncidents/app/src/main/java/fr/epsi/alerteincidents/IncidentActivity.java
@@ -0,0 +1,126 @@
+package fr.epsi.alerteincidents;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import classes.metier.Incident;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import fr.epsi.helper.Webservice;
+
+public class IncidentActivity extends Activity {
+	// used to store app title
+	private CharSequence mTitle;
+	private final String tag = "IncidentActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_incident);
+        //change le titre de l'activite
+        setTitle("Ajouter un Incident");        
+        
+	}
+	
+	//gestion bouton retour
+	public void onBackPressed(){
+		//recupere le nom de l'activite precedente
+		SharedPreferences mPrefs = getSharedPreferences("ActivityName", Context.MODE_PRIVATE);
+		String current = mPrefs.getString("ActivityName", "NoName");
+		Log.v("onBackPressed",current);
+		
+        //donne le nom de l'activite
+		Editor edit = mPrefs.edit();
+		edit.putString("ActivityName", "IncidentActivity");
+		edit.commit();
+		
+		//test du nom de l'activite
+		if (current.equals("MainActivity")){
+			Intent intent = new Intent(this,MainActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+		if (current.equals("HistoriqueActivity")){
+			Intent intent = new Intent(this,HistoriqueActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+		if (current.equals("CarteActivity")){
+			Intent intent = new Intent(this,CarteActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+		if (current.equals("PreferencesActivity")){
+			Intent intent = new Intent(this,PreferencesActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+		
+        int id = item.getItemId();
+        if (id == R.id.action_main) {
+			Intent intent = new Intent(this,MainActivity.class);
+			startActivity(intent);
+			this.finish();
+            return true;
+        }
+        if (id == R.id.action_carte) {
+			Intent intent = new Intent(this,CarteActivity.class);
+			startActivity(intent);
+			this.finish();
+            return true;
+        }
+        if (id == R.id.action_historique) {
+			Intent intent = new Intent(this,HistoriqueActivity.class);
+			startActivity(intent);
+			this.finish();
+            return true;
+        }
+        if (id == R.id.action_incident) {
+			Intent intent = new Intent(this,IncidentActivity.class);
+			startActivity(intent);
+			this.finish();
+            return true;
+        }
+        if (id == R.id.action_preferences) {
+			Intent intent = new Intent(this,PreferencesActivity.class);
+			startActivity(intent);
+			this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+	
+	//set app title on actionBar
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+}
